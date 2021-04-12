ead8f0e47a08873c6d8cd1cb23ee2dda7ddaabbc
YanaLugina <yana4961@gmail.com>
2021-04-13 00:47:47 +1000

    future: added late for users List, and fixed names         изменено:      lib/event_provider.dart         изменено:      lib/user_provider.dart

--- lib/user_provider.dart
+++ lib/user_provider.dart
@@ -1,19 +1,21 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 import 'package:flutter/services.dart';
 
 import 'models/user.dart';
+import 'models/user_list.dart';
 
 class UserProvider {
   final String _dataPath = 'asserts/user.json';
-  List<User> users;
-  Future<String> loadAssert() async {
-    return await Future.delayed(Duration(seconds: 10),  () async {
+  late List<User> users;
+
+  Future<String> loadAsset() async {
+    return await Future.delayed(Duration(seconds: 10), () async {
       return await rootBundle.loadString(_dataPath);
     });
   }
 
   Future<List<User>> loadUserData() async {
-    var dataString = await loadAssert();
+    var dataString = await loadAsset();
     Map<String, dynamic> jsonUserData = jsonDecode(dataString);
     users = UserList.fromJson(jsonUserData['users']).users;
     print('done loading user!' + jsonEncode(users));
